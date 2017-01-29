ae75df7fd04e9140ebc567e1e2173d6b97aa9b51
x1nixmzeng <x1nixmzeng@live.co.uk>
2017-01-29 22:10:46 +0000

    Added CRC32 checksum

---
+++ RevEngGlue/Checksum.cs
@@ -0,0 +1,48 @@
+﻿namespace RevEngGlue
+{
+    public enum Checksum
+    {
+        BYTE,
+        SHORT_LE,
+        SHORT_BE,
+        INT_LE,
+        INT_BE,
+        INT64_LE,
+        INT64_BE,
+        SUM8,
+        SUM16,
+        SUM32,
+        SUM64,
+        CRC16,
+        CRCCCITT,
+        CRC32,
+        ADLER32,
+    }
+
+    public enum ChecksumAlg
+    {
+        BYTE,
+        SHORT_LE,
+        SHORT_BE,
+        INT_LE,
+        INT_BE,
+        INT64_LE,
+        INT64_BE,
+        SUM8,
+        SUM16,
+        SUM32,
+        SUM64,
+        CRC16,
+        CRCCCITT,
+        CRC32,
+        ADLER32,
+        MD2,
+        MD4,
+        MD5,
+        RIPEMD160,
+        SHA1,
+        SHA256,
+        SHA512,
+        TIGER,
+    }
+}
---
+++ RevEngGlue/ChecksumImpl.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using DamienG.Security.Cryptography;
+
+namespace RevEngGlue
+{
+    public class ChecksumImpl
+    {
+        /// <summary>
+        /// Calculate the CRC32 checksum from an array of bytes
+        /// </summary>
+        public static uint crc32(byte[] data)
+        {
+            return Crc32.Compute(data);
+        }
+
+        /// <summary>
+        /// Calculate the CRC32 checksum from a string
+        /// </summary>
+        public static uint crc32(string data)
+        {
+            return crc32(data, CharSet.ANSI);
+        }
+
+        /// <summary>
+        /// Calculate the CRC32 checksum from a string with a given encoding
+        /// </summary>
+        public static uint crc32(string data, Encoding enc)
+        {
+            return Crc32.Compute(enc.GetBytes(data));
+        }
+    }
+}
